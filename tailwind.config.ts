abca7ef1f5404c558538bcba8544be8cb05b5643
Carson Price <75814830+CPRocker@users.noreply.github.com>
2024-08-16 20:47:28 -0500

    Fix Tailwind Config (#5)
    Fix tailwind config import path

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import { type Config } from "./node_modules/.deno/tailwindcss@3.4.1/node_modules/tailwindcss/types/index.js";
+import { type Config } from "./node_modules/.deno/tailwindcss@3.4.1/node_modules/tailwindcss/types/index.d.ts";
 
 export default {
   content: ["{routes,islands,components}/**/*.{ts,tsx}"],
