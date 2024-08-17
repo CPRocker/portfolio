82cd149f765569606e4992cb74498e610b79c00b
Carson Price <carsondp02@gmail.com>
2024-08-16 22:19:11 -0500

    Remove fresh boilerplate

--- routes/_404.tsx
+++ routes/_404.tsx
@@ -6,16 +6,16 @@ export default function Error404() {
       <Head>
         <title>404 - Page not found</title>
       </Head>
-      <div class="px-4 py-8 mx-auto bg-[#86efac]">
+      <div class="px-4 py-8 mx-auto bg-sky-300">
         <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
           <img
             class="my-6"
             src="/logo.svg"
             width="128"
             height="128"
-            alt="the Fresh logo: a sliced lemon dripping with juice"
+            alt="Carson Price logo: a C with a slash and a P"
           />
-          <h1 class="text-4xl font-bold">404 - Page not found</h1>
+          <h1 class="text-4xl font-bold">404 - Whoops!</h1>
           <p class="my-4">
             The page you were looking for doesn't exist.
           </p>
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,24 +1,18 @@
-import { useSignal } from "@preact/signals";
-import Counter from "../islands/Counter.tsx";
-
 export default function Home() {
-  const count = useSignal(3);
   return (
-    <div class="px-4 py-8 mx-auto bg-[#86efac]">
+    <div class="px-4 py-8 mx-auto bg-sky-300">
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
         <img
           class="my-6"
           src="/logo.svg"
           width="128"
           height="128"
-          alt="the Fresh logo: a sliced lemon dripping with juice"
+          alt="Carson Price logo: a C with a slash and a P"
         />
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
+        <h1 class="text-4xl font-bold">Welcome to carsonprice.me</h1>
         <p class="my-4">
-          Try updating this message in the
-          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
+          This site is dedicated to my life as a developer.
         </p>
-        <Counter count={count} />
       </div>
     </div>
   );
