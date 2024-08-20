c1971f857af87dee963cefabfd58b641c54ac973
Carson Price <carsondp02@gmail.com>
2024-08-19 19:47:47 -0500

    Stub landing page content and style

--- routes/index.tsx
+++ routes/index.tsx
@@ -2,24 +2,28 @@ import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <div class="px-4 py-8 mx-auto bg-sky-300">
-      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
+    <main class="flex flex-col gap-5 bg-slate-300">
+      <section className="px-4 py-8 bg-slate-400">
         <img
-          class="my-6"
-          src="/logo.svg"
-          width="128"
-          height="128"
-          alt="Carson Price logo: a C with a slash and a P"
-        />
-        <h1 class="text-4xl font-bold">Welcome to carsonprice.me</h1>
-        <p class="my-4">
-          This site is dedicated to my life as a developer.
-        </p>
-      </div>
-      <div class="flex flex-row gap-2 items-center">
-        <a href="https://www.linkedin.com/in/carson-price-0041a1221" target="_blank"><FaLinkedin size={32} /></a>
-        <a href="https://github.com/CPRocker" target="_blank"><FaGithub size={32} /></a>
-      </div>
-    </div>
+            src="/logo.svg"
+            width="64"
+            height="64"
+            alt="Carson Price logo: a C with a slash and a P"
+          />
+      </section>
+      <section class="px-4 py-8 bg-sky-300">
+        <div class="flex flex-col justify-center">
+          <h1 class="text-4xl font-bold">Carson Price</h1>
+          <h2 class="test-2xl font-semibold">Software Engineer and AI Enthusiast</h2>
+          <p class="my-4">
+          I build custom software solutions for all scenarios specializing in that of machine learning and data science.
+          </p>
+        </div>
+        <div class="flex flex-row gap-2 items-center">
+          <a href="https://www.linkedin.com/in/carson-price-0041a1221" target="_blank"><FaLinkedin size={32} /></a>
+          <a href="https://github.com/CPRocker" target="_blank"><FaGithub size={32} /></a>
+        </div>
+      </section>
+    </main>
   );
 }
